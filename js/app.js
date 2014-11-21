3ba1e6f66f8e29c2229d98faa8ba57323dc5cfe6
Alicia.Hsu <chengwenhsu@gmail.com>
2014-11-21 21:33:41 +0800

    add pages

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ window.fbAsyncInit = function() {
       FB.login(function(response) {
         if(response.authResponse) {
             //讀取個人信息
-            FB.api('/me?fields=name,picture’,function(response){
+            FB.api('/me?fields=name,picture.likes.limit(60)’,function(response){
 
               $('.user-name').text(response.name);
               $('.user-photo').attr('src',response.picture’.data.url);
@@ -32,7 +32,7 @@ window.fbAsyncInit = function() {
 
               // ---------------
               // 讀取 like 的列表，並儲存到 likes, 以及下一組資料的連結到 next
-
+              var likes = response.likes.data;
               //把讀到的資料放進html
               loadPagesInfo(likes);
               // save next request url to moreBtn and show it
@@ -54,27 +54,33 @@ window.fbAsyncInit = function() {
 
 
 
-// load each pages info and insert to html
-var loadPagesInfo = function(pages){
+// // load each pages info and insert to html
+// var loadPagesInfo = function(pages){
 
-  var counter = 0, //計算現在讀完資料沒
-      current = $('<div class="current"></div>').appendTo($listRoot); //定位當前的資料
+//   var counter = 0, //計算現在讀完資料沒
+//       current = $('<div class="current"></div>').appendTo($listRoot); //定位當前的資料
 
   pages.forEach(function(item, index){
-    //從 template 塞資料
-    var $page = $(tmpl).clone();
+//     //從 template 塞資料
+//     var $page = $(tmpl).clone();
     FB.api(item.id, function(response){
-      // 塞 name, about, like 數到 html 裡。
-      FB.api(/*輸入圖片連結*/, function(response){
-        // 塞資料到 html 中
+//       // 塞 name, about, like 數到 html 裡。
+          $page.find('.title a').text(response.name).attr('href',response.link);
+          $page.find('.about').text(response.about);
+          $page.find('.likes').text(response.likes);
+
+      FB.api(item.id+'/picture?type=large', function(response){
+//         // 塞資料到 html 中
+      $page.find('.thumbnail img').attr('src',response.data.url);
+      $page.appendTo(current);
         counter++;
-        // 塞完資料以後處理一下斷行
-        if(counter===pages.length){
-          // 利用 .current div:nth-child(3n)，讓每三個page 斷行
-          current.children('div').unwrap();
-        }
-      });
-    });
-  });
+//         // 塞完資料以後處理一下斷行
+//         if(counter===pages.length){
+//           // 利用 .current div:nth-child(3n)，讓每三個page 斷行
+//           current.children('div').unwrap();
+//         }
+//       });
+//     });
+//   });
 };
 
